6b6046743f605975a81f4758bd9f22248371a554
Romain Forestier <romain.forestier@utas.edu.au>
2021-10-29 11:19:01 +1100

    adding composer for hostinger deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1adc119ca2f22c5e8b7cdc441308a3a0723d52f1',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1adc119ca2f22c5e8b7cdc441308a3a0723d52f1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
